c3572c567d7a0d79744cb17e2720bfe8a0b4dfe9
SinanChiraqov <62188808+SinanChiraqov@users.noreply.github.com>
2022-09-04 02:31:14 -0700

    Add files via upload

--- js slider/main.js
+++ js slider/main.js
@@ -1,36 +1,77 @@
-let images = ["./images/1.jpg", "./images/2.png", "./images/3.jpg", "./images/4.jpg", "./images/5.jpg"];
 let prewBtn = document.getElementById("prew");
 let nextBtn = document.getElementById("next");
-let sliderBox = document.querySelector(".slider-box");
+let sliderBox = document.querySelector(".slider__box");
+let sliderTitle = document.querySelector(".slider__title");
+let sliderSubTitle = document.querySelector(".slider__subtitle");
 
 prewBtn.addEventListener("click", prewSlide);
 nextBtn.addEventListener("click", nextSlide);
 
-let i = 1;
-sliderBox.innerHTML = `<img src="${images[i]}" alt="" />`;
+let data = [
+  {
+    id: 0,
+    image: "./images/2.png",
+    title: "Lorem, ipsum.",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id. Rerum maiores quo nobis dolorum provident consectetur soluta fugiat tenetur quae aut!",
+  },
+  {
+    id: 1,
+    image: "./images/3.jpg",
+    title: "ipsum.",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id. Rerum maiores quo nobis dolorum provident consectetur soluta fugiat tenetur quae aut! Quidem, alias esse laboriosam eaque unde, dolorum",
+  },
+  {
+    id: 2,
+    image: "./images/4.jpg",
+    title: "Lorem.",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id. Rerum maiores quo nobis dolorum provident consectetur soluta fugiat tenetur quae aut! Quidem, alias esse laboriosam eaque unde, dolorum, ad iusto vero cumque voluptate veniam quasi nobis pariatur dolores!",
+  },
+  {
+    id: 3,
+    image: "./images/5.jpg",
+    title: "Lorem, ipsum sit.",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id. Rerum maiores quo nobis dolorum provident consectetur soluta fugiat tenetur quae aut! Quidem, alias esse laboriosam eaque unde, dolorum, ad iusto vero cumque voluptate veniam quasi nobis pariatur dolores!",
+  },
+];
+
+let i = 0;
+
+function slide(index) {
+  var item = data.find((item) => item.id === index);
+  sliderBox.style.backgroundImage = `url(${item.image})`;
+  sliderTitle.innerHTML = `${item.title}`;
+  sliderSubTitle.innerHTML = `${item.subtitle}`;
+}
 function nextSlide(a) {
   i++;
-  sliderBox.innerHTML = `<img src="${images[i]}" alt="" />`;
-  if (i==images.length-1) {
-    i=0
+  if (i > data.length - 1) {
+    i = 0;
   }
+  slide(i);
 }
-
 
 function prewSlide(a) {
+  slide(i);
   i--;
-  sliderBox.innerHTML = `<img src="${images[i]}" alt="" />`;
-  if (i==0) {
-    let lastImgValue = images.length - 1;
-    console.log(lastImgValue)
-    i=4;
+  if (i < 0) {
+    i = data.length - 1;
   }
 }
 
-setInterval(() => {
-  i++;
-  sliderBox.innerHTML = `<img src="${images[i]}" alt="" />`;
-  if (i==images.length-1) {
-    i=0
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 37) {
+    e.keypress = "1";
+    prewSlide();
   }
-}, 3000)
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 39) {
+    e.keypress = "1";
+    prewSlide();
+  }
+});
